182bdef2ff09d5e295c71eff84a91c87a12077c0
OvernightSolo <sterlingtx75@gmail.com>
2022-08-20 13:32:54 -0500

    add some questions

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 var inquirer = require("inquirer");
 const fs = require("fs");
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const questions = [
+  {
+    type: "input",
+    name: "project_title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project",
+  },
+  {
+    type: "input",
+    name: "table-of-contents",
+    message: "Which items would you like to be in the Table of Contents?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "How do you use this application?",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "What are the contribution guidelines?",
+  },
+  {
+    type: "input",
+    name: "test-instructions",
+    message: "What are the test instructions?",
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  console.log(JSON.stringify(answers, null, "  "));
+});
